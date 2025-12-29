f97b75cd85496ba8fd80609a49ab1af5c385f112
paulo2tab <paulo@cartoriocriciuma.com.br>
2025-12-29 14:39:42 -0300

    feat: Adicionado as telas de Relatório e Update em massa das certidões.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    // Allow sentence case in commit subjects (e.g., "Atualizado as diretrizes do husky").
+    "subject-case": [0],
+  },
 };
